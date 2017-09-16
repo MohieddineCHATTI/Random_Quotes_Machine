7efd144585f1824f8a1defda817ac300498d0fa2
MohieddineCHATTI <mohieddinechatti@gmail.com>
2017-09-16 18:41:06 +0200

    random quote machine before making tweeter btn

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        function get_quote(){
+        var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+        $.getJSON(url, function(data){
+            $(".quote").html(data.quoteText);
+            $(".author").html(" -- "+data.quoteAuthor+" -- ");
+        })
+    }
+$(".new_quote").on("click", function(){
+    get_quote();
+})
+})
