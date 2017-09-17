5cb6200bd45542311d590bed7b96dc6c4465db0f
MohieddineCHATTI <mohieddinechatti@gmail.com>
2017-09-17 11:26:48 +0200

    RQM with twitter button --not responsive--

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
         function get_quote(){
         var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
         $.getJSON(url, function(data){
-            $(".quote").html(data.quoteText);
+            $(".quote").html('" '+data.quoteText+' "');
             $(".author").html(" -- "+data.quoteAuthor+" -- ");
+            $(".twitter_btn").on("click", function(){
+                window.open("https://twitter.com/intent/tweet?text=" +data.quoteText+"author :   "+data.quoteAuthor);
+            })
         })
     }
 $(".new_quote").on("click", function(){
